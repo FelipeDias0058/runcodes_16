c5b39fa0d83c2f7a8f37eddaf37cd204fb0a6363
FelipeDias0058 <143054608+FelipeDias0058@users.noreply.github.com>
2023-10-23 11:10:00 -0300

    5_parcela_sem_juros.py

--- 5_parcela_sem_juros.py
+++ 5_parcela_sem_juros.py
@@ -1,6 +1,6 @@
 #Calcula o valor da prestação com base no valor da compra
 def f_prestacao(v_compra):
-    for p in range(1, 23):
+    for p in range(1, 25):
         prestacao = v_compra / p
         print(f'{p}x de R$ {prestacao:.2f}')
 
